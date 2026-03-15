b1a7aaadb59b1e7bf652dea2864cb862a10702cb
Süleyman Konak <suleymankonak261@gmail.com>
2026-03-15 21:47:13 +0300

    Güvenlik Güncellemesi Yapıldı

--- src/components/FirebaseMain.jsx
+++ src/components/FirebaseMain.jsx
@@ -2,17 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
+// !!! GERÇEK DEĞERLERİ BURAYA KOYMAYIN !!!
+// Buraya kendi Firebase projenizin bilgilerini koyacaksınız
 const firebaseConfig = {
-    apiKey: "AIzaSyA0-AcmxqlZMRbRX5KppRnitOx0hCtHb4M",
-    authDomain: "fir-learning-59e82.firebaseapp.com",
-    projectId: "fir-learning-59e82",
-    storageBucket: "fir-learning-59e82.firebasestorage.app",
-    messagingSenderId: "282740497014",
-    appId: "1:282740497014:web:34a40bad3b1a1b3bc59d57",
-    measurementId: "G-C1MX18B3PP"
+    apiKey: "BURAYA_FIREBASE_API_KEY",            // Örn: "AIzaSyA0-xxxxxxxxxxxxxxxxxx"
+    authDomain: "BURAYA_FIREBASE_AUTH_DOMAIN",    // Örn: "proje-adi.firebaseapp.com"
+    projectId: "BURAYA_FIREBASE_PROJECT_ID",      // Örn: "proje-adi"
+    storageBucket: "BURAYA_FIREBASE_STORAGE_BUCKET", // Örn: "proje-adi.appspot.com"
+    messagingSenderId: "BURAYA_FIREBASE_MESSAGING_SENDER_ID", // Örn: "1234567890"
+    appId: "BURAYA_FIREBASE_APP_ID",              // Örn: "1:1234567890:web:abcdef123456"
+    measurementId: "BURAYA_FIREBASE_MEASUREMENT_ID" // Örn: "G-XXXXXXX"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
-export const auth = getAuth(app); 
+export const auth = getAuth(app);
